f7c489fe33fa0059ef11a6cb19da5cd6d6d766a2
Linh Tung <linhtung@Linhs-Mac-mini.local>
2026-08-18 09:20:43 +0700

    feat: optimize mobile navigation drawer for iPhone and clean up redundant files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/styles.css'
-import AppClone from './AppClone.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppClone />
+    <App />
   </StrictMode>,
 )
